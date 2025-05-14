227d9c05665144f0b476cd448bc0a1e5629237ba
khytryy <yehor.khytyy@gmail.com>
2025-05-14 17:11:18 +0200

    norOS: renamed from source to arch

---
+++ arch/c/lib/drivers/fb/fb.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdint.h>
+#include "../../multiboot2/mb2.h"
+#include "../serial/serial.h"
+#include <stddef.h>
+
+
+extern uint32_t* framebuffer;
+extern uint32_t fb_width;
+extern uint32_t fb_height;
+extern uint8_t fb_bpp;
+extern uint32_t fb_pitch;
+
+void fbInit(void* pointer);
+void fbplot(int x, int y, uint32_t color);
+void test_framebuffer_access();
+uint32_t fbread(int x, int y);
